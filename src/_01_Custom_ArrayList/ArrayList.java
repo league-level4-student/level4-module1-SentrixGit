0568f7df30bc40da19e7ac6fc617e183002ffa59
sen_trix <44371257+sentrixd@users.noreply.github.com>
2020-09-29 19:54:34 -0700

    ddd
    ddd

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -2,18 +2,38 @@ package _01_Custom_ArrayList;
 
 @SuppressWarnings("unchecked")
 
-public class ArrayList <T>{
+public class ArrayList <T> {
+	T[] customArray;
+	
+	public <Template> Template calcStuff(Template t, int r) {
+		for (int i = 0; i < r; i++) {
+			System.out.println(t);
+		}
+		
+		return t;
+	}
+	
+	
 	
 	public ArrayList() {
+		customArray = (T[]) new Object[0];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
 		
-		return null;
+		return customArray[loc];
 	}
 	
 	public void add(T val) {
+		T[] temp = customArray;
 		
+		customArray = (T[]) new Object[temp.length + 1];
+		
+		for (int i = 0; i < temp.length; i++) {
+			customArray[i] = temp[i];
+		}
+		
+		customArray[customArray.length - 1] = val;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
